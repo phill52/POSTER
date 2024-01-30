8603442da9af72317be4ce27682708f28f683994
phill52 <paner225@gmail.com>
2024-01-30 15:04:54 -0500

    init

---
+++ src/renderer/src/utils/RequestTypes.tsx
@@ -0,0 +1,13 @@
+enum HttpRequestType {
+  GET = 'GET',
+  HEAD = 'HEAD',
+  POST = 'POST',
+  PUT = 'PUT',
+  PATCH = 'PATCH',
+  DELETE = 'DELETE',
+  OPTIONS = 'OPTIONS',
+  TRACE = 'TRACE',
+  CONNECT = 'CONNECT'
+}
+
+export default HttpRequestType
